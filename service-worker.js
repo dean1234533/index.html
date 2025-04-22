0487004842c23302e3bf50f3a3af22e0d1e15f9d
dean1234533 <deanburt1308@gmail.com>
2025-04-22 01:07:50 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,25 @@
 self.addEventListener('install', event => {
-    event.waitUntil(
-      caches.open('dbworkouts-cache').then(cache => {
-        return cache.addAll([
-          '/',
-          '/index.html',
-          '/manifest.json',
-          '/icons/icon-192x192.png',
-          '/icons/icon-512x512.png'
-        ]);
-      })
-    );
-  });
+  event.waitUntil(
+    caches.open('dbworkouts-cache').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json',
+        '/icons/icon-192x192.png',
+        '/icons/icon-512x512.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
 
   self.addEventListener('fetch', event => {
     event.respondWith(
